669405eac336103b132e74c42eb0244a6639158b
Serhii <seleshhenko@yahoo.com>
2017-10-06 16:19:13 +0300

    done

---
+++ js/common.js
@@ -0,0 +1,32 @@
+$(function() {
+    
+
+
+
+    $('#tabs').children('li').first().children('a').addClass('active-tab').next().addClass('is-open').show();
+
+    $('#tabs').on('click', 'li > a', function() {
+
+        if (!$(this).hasClass('active-tab')) {
+
+            $('#tabs .is-open').removeClass('is-open').hide();
+            $(this).next().toggleClass('is-open').toggle();
+
+            $('#tabs').find('.active-tab').removeClass('active-tab');
+            $(this).addClass('active-tab');
+        } else {
+            $('#tabs .is-open').removeClass('is-open').hide();
+            $(this).removeClass('active-tab');
+        }
+    });
+
+        var url = window.location.href;
+        if(url === 'http://localhost:3000/'){
+            history.pushState("", "", "http://localhost:3000/#General");
+        }
+        else if (window.location.hash.length > 0) {
+        $('a[href="' + window.location.hash + '"]').click();
+    };
+
+
+});
